f4c9d55c45a35e797057c903693ca546f38f30b7
Vsevolod Seitov <vsevolodseitov@gmail.com>
2023-09-30 04:41:22 +0300

    Почему-то вебшторм решил, что я вносик какие-то изменения в эти файлы

--- js/sliders-inicialization.js
+++ js/sliders-inicialization.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    new ItcSimpleSlider('#slider-1', {
-      loop: true,
-      autoplay: true,
-      interval: 10000,
-      swipe: true,
-    });
+  new ItcSimpleSlider('#slider-1', {
+    loop: true,
+    autoplay: true,
+    interval: 10000,
+    swipe: true,
   });
-  document.addEventListener('DOMContentLoaded', () => {
-    new ItcSimpleSlider('#slider-2', {
-      loop: true,
-      autoplay: true,
-      interval: 5000,
-      swipe: false,
-    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+  new ItcSimpleSlider('#slider-2', {
+    loop: true,
+    autoplay: true,
+    interval: 5000,
+    swipe: false,
   });
+});
